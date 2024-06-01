625b36a5490ab4ede3b3cd4369de10ae715e2cbf
Edward-Precious Omegbu <eomegbu@gmail.com>
2024-06-01 17:19:07 +0100

    functionality complete

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,10 @@ const planetSelect = document.querySelector(".select");
 const btn = document.querySelector("button");
 const massInput = document.querySelector("input");
 const imageEl = document.querySelector(".planet-image");
+const textEl = document.querySelector("#text");
+const textContainer = document.querySelector("#text-container");
+const imageContainer = document.querySelector("#img-container");
+const weightEl = document.querySelector("#weight");
 
 // const planets = [
 //     "Mercury",
@@ -15,10 +19,12 @@ const imageEl = document.querySelector(".planet-image");
 //     "Neptune",
 //     "Pluto"
 // ]
+
+// weight = mass * acc;
 const planets = {
     "MERCURY": {image: "./images/mercury.png", acc: 5},
     "VENUS": {image: "./images/venus.png", acc: 5},
-    "EARTH": {image: "./images/earth.png", acc: 5},
+    "EARTH": {image: "./images/earth.png", acc: 9.8},
     "MOON": {image: "./images/moon.png", acc: 5},
     "MARS": {image: "./images/mars.png", acc: 5},
     "JUPITER": {image: "./images/jupiter.png", acc: 5},
@@ -45,15 +51,39 @@ function createSelectOptions() {
 }
 
 function clickBtn() {
-    const mass = massInput.value;
+    const mass = Number(massInput.value);
     const planet = planetSelect.value; 
     const image = planets[planet]?.image || "";
+    const acc = planets[planet]?.acc || 0;
 
-    // const imageContainer = document.querySelector(".flex-item.image");
-    // imageContainer.innerHTML = `
-    //     <img src="${image}" class="planet-image" />
-    // `
-    imageEl.setAttribute("src", image);
+    textContainer.classList.replace("hidden", "flex");
+ 
+    if (mass && image && acc) {
+        imageContainer.classList.replace("hidden", "flex");
+        imageEl.setAttribute("src", image);
+
+        const weight = mass * acc;
+        weightEl.innerHTML = `<span>${weight} N</span>`;
+        weightEl.classList.replace("hidden", "grid");
+        
+        textEl.innerHTML = `The weight of the object on <span style="font-weight: 700;">${planet}</span>`;
+        console.log("Everything okay for calculation. ", "Weight is ", weight);
+    } 
+    
+    if (!planet) {
+        imageContainer.classList.replace("flex", "hidden");
+        textEl.innerHTML = "You did not choose a planet yet";
+        weightEl.classList.replace("grid", "hidden");
+        console.log("No planet");
+    }
+    
+    if (!mass) {
+        textEl.innerHTML = "Mass is required";
+        weightEl.classList.replace("grid", "hidden");
+        console.log("No mass");
+    } 
+    
+    // imageEl.setAttribute("src", image);
 
     console.log({mass, planet, image});
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./**/*.{html, js}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        "2xl": "1536px"
+      },
+      backgroundImage: {
+        galaxy: "url(./images/galaxy.gif)"
+      },
+      spacing: {
+        giyu: "3.425rem"
+      }
+    },
   },
   plugins: [],
 }
